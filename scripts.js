8e689c9095bf5e420c4377a6a4102323fd398642
unknown <rakibulhasan.seu.cse@gmail.com>
2022-08-07 01:20:03 +0600

    getting email and pass

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function(){
+    let email = document.getElementById("userEmail");
+    let password = document.getElementById("userPassword");
+
+    console.log(email.value);
+    console.log(password.value);
+})
